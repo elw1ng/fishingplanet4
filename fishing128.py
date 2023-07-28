bb56898ed522ae588a7cc7c68746d2aa66610f84
Dziema <motorollllo@gmail.com>
2023-07-28 18:13:35 +0300

    filter

--- fishing128.py
+++ fishing128.py
@@ -492,7 +492,7 @@ class ClassName(BaseScript):  # Название класса (должен от
 
                     else:
                         counter =0
-                    if counter>5:
+                    if counter>15:
                         print("GOT IT")
                         self.lkmrelease()
                         sleep(0.5)
